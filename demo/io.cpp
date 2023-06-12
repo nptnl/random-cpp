f1e1f7fa8bdbb8b284c5ab638821e7140d770474
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-12 14:10:53 -0400

    cout cout
    cin cin

---
+++ demo/io.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int some;
+    cout << "eat shift\n";
+    cin >> some;
+    cout << some;
+    return 0;
+}
---
+++ demo/poiunt.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char eating[] = "eating some food i reckon\n";
+    cout << &eating << '\n';
+    cout << eating;
+    return 0;
+}
