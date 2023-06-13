7c35e27d3dd3eda81c13a6a9e7ce9323e3e01ee1
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-13 09:37:31 -0400

    operator overload
    im not doing this anymore

--- demo/class.cpp
+++ demo/class.cpp
@@ -6,12 +6,22 @@ class Comp {
     double r;
     double i;
 
+    Comp() { r = 0.; i = 0.; }
     Comp(double re, double im) { r = re; i = im; }
     double mag_square() { return r*r + i*i; }
+
+    Comp operator+ (const Comp&);
 };
+Comp Comp::operator+ (const Comp& other) {
+    Comp ret;
+    ret.r = r + other.r;
+    ret.i = i + other.i;
+    return ret;
+}
 
 int main() {
     Comp x (3.0, 4.0);
-    cout << x.mag_square();
+    Comp y (5.0, 6.0);
+    cout << (x + y).mag_square();
     return 0;
 }
